3cb6dec80090a9276ea7fb7478a09201543ef19f
Ciremy <ciremy@MacBook-Air-de-Ciremy.local>
2022-12-12 10:45:51 +0100

    update

--- app.js
+++ app.js
@@ -1,16 +1,20 @@
 const express = require("express");
+const { stdout } = require("process");
 const app = express();
 const port = 8000;
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });
-
+const usingPortMinecraft = 25565
+let serverList = []
 app.get("/launch", (req, res) => {
   const subProcess = require("child_process");
   subProcess.exec(
     // "docker build -t getting-started . &&
-    `docker run -d -p 300:300 nginx`,
+
+    `docker run -d -it -p ${usingPortMinecraft + serverList.length}:${usingPortMinecraft + serverList.length} -e EULA=TRUE --name mc${numbreOfServer.length} itzg/minecraft-server`,
+
     (err, stdout, stderr) => {
       if (err) {
         console.error(err);
@@ -21,10 +25,36 @@ app.get("/launch", (req, res) => {
       }
     }
   );
-  res.send("launching....");
+  res.send(`launching on port :${usingPortMinecraft + numbreOfServer[-1]}`);
+  
 });
 
-app.get("/close", (req, res) => {
+app.post("/close", (req, res) => {
+  const subProcess = require("child_process");
+
+  let numbreOfServerPost
+  
+  !!req.body.server ? numbreOfServerPost = req.body.server  : numbreOfServerPost = numbreOfServer;
+
+  //var numbreOfServerPost = req.body.server;
+  subProcess.exec(
+    // "docker build -t getting-started . &&
+    `docker stop mc${numbreOfServerPost} && docker rm mc${numbreOfServerPost} --force`,
+    (err, stdout, stderr) => {
+      if (err) {
+        console.error(err);
+        process.exit(1);
+      } else {
+        console.log(`The stdout Buffer from shell: ${stdout.toString()}`);
+        console.log(`The stderr Buffer from shell: ${stderr.toString()}`);
+      }
+    }
+  );
+  res.send("closing everything ");
+  numbreOfServer --;
+});
+
+app.get("/closeAll", (req, res) => {
   const subProcess = require("child_process");
   subProcess.exec(
     // "docker build -t getting-started . &&
